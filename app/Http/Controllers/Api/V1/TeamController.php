9f5ee02ed1fefc0d796650f7f1436eb92c2845b1
Hasan_ab <aberzakhasan@hotmail.com>
2022-11-13 14:50:12 +0100

    create teams

---
+++ app/Http/Controllers/Api/V1/TeamController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\V1\TeamCollection;
+use App\Http\Resources\V1\TeamResource;
+use App\Models\Team;
+use Illuminate\Http\Request;
+use App\Http\Requests\V1\StoreTeamRequest;
+
+class TeamController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return new TeamCollection( Team::all());
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Team  $team
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Team $team)
+    {
+        return new TeamResource($team);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreTeamRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreTeamRequest $request)
+    {
+        return new TeamResource(Team::create($request->all()));
+    }
+}

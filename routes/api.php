7a6a0f3de9183400e419d96b6765c9e1b5194d44
Hasan_ab <aberzakhasan@hotmail.com>
2022-11-14 21:52:10 +0100

    add search to pokemons

--- routes/api.php
+++ routes/api.php
@@ -22,10 +22,12 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::group(['prefix' => 'v1', 'namespace'=>'App\Http\Controllers\Api\V1'],function(){
     Route::get('pokemons', [PokemonController::class, 'index']);
-    Route::get('pokemons/{pokemon}', [PokemonController::class, 'show']);
+    Route::get('pokemons/{pokemon}', [PokemonController::class, 'show'])->whereNumber('id');
     Route::apiResource('teams', TeamController::class)->only([
         'index','store'
     ]);
-    Route::get('teams/{team}', [TeamController::class,'show']);
+    Route::get('teams/{team}', [TeamController::class,'show'])->whereNumber('id');
     Route::post('teams/{team}', [TeamController::class,'storeTeamPokemon']);
+
+    Route::get('search', [PokemonController::class, 'search']);
 });

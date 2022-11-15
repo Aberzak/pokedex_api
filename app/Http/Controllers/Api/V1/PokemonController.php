b02cf6c0dfebd3d6b10c5f67721c596650c53d31
Hasan_ab <aberzakhasan@hotmail.com>
2022-11-15 19:45:48 +0100

    adjust Pokemon and controller

--- app/Http/Controllers/Api/V1/PokemonController.php
+++ app/Http/Controllers/Api/V1/PokemonController.php
@@ -38,16 +38,6 @@ class PokemonController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Display the specified resource.
      *
      * @param  \App\Models\Pokemon  $pokemon

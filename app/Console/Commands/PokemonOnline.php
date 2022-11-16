79892455daa3a2ff01d5cbbfd23fed52c49bd226
Hasan_ab <aberzakhasan@hotmail.com>
2022-11-16 14:09:00 +0100

    Add command to import pokemon from online

--- app/Console/Commands/PokemonOnline.php
+++ app/Console/Commands/PokemonOnline.php
@@ -2,7 +2,19 @@
 
 namespace App\Console\Commands;
 
+use App\Models\Abilitie;
+use App\Models\Move;
+use App\Models\MoveLearnMethod;
 use App\Models\Pokemon;
+use App\Models\PokemonAbilitie;
+use App\Models\PokemonMove;
+use App\Models\PokemonStat;
+use App\Models\PokemonType;
+use App\Models\Sprite;
+use App\Models\Stat;
+use App\Models\Type;
+use App\Models\VersionGroup;
+use App\Models\VersionGroupDetail;
 use Illuminate\Console\Command;
 use PokePHP\PokeApi;
 
@@ -31,10 +43,154 @@ class PokemonOnline extends Command
     {
         $api = new PokeApi;
         $pokemonJson= $api->pokemon($this->argument('pokemon'));
-        $pokemonsArray = json_decode($pokemonJson,true);
-        
-        dd($pokemonsArray);
 
+        if($pokemonJson == '"An error has occured."'){
+
+            $this->error('This Pokemon Does Not Exist');
+            return Command::FAILURE;
+        }
+
+        $pokemonData = json_decode($pokemonJson,true);
+
+        $abilities = [];
+        $moves = [];
+        $moveLearnMethods = [];
+        $versionGroups = [];
+        $types = [];
+        $stats = [];
+        
+        $pokemon = Pokemon::firstOrCreate(
+            [
+                'id' => $pokemonData['id'],
+                'name' => $pokemonData['name'],
+                'height' => $pokemonData['height'],
+                'weight' => $pokemonData['weight'],
+                'order' => $pokemonData['order'],
+                'form' => $pokemonData['forms'][0]['url']
+            ]
+            );
+
+        foreach ($pokemonData['abilities'] as $abilitieData){
+
+            if (!isset($abilities[$abilitieData['ability']['name']])){
+                $ability = Abilitie::firstOrCreate(
+                    ['name' => $abilitieData['ability']['name'],
+                    'url' => $abilitieData['ability']['url']]
+                );
+                $abilities[$ability->name] = $ability;
+
+            }else{
+                $ability = $abilities[$abilitieData['ability']['name']];
+            
+            }
+
+            PokemonAbilitie::firstOrCreate([
+                'pokemon_id' => $pokemon->id,
+                'slot' => $abilitieData['slot'],
+                'is_hidden' => $abilitieData['is_hidden'],
+                'abilitie_id' => $ability->id
+            ]);
+        }
+               
+        foreach ($pokemonData['moves'] as $moveData){
+         if (!isset($moves[$moveData['move']['name']])){
+            $move = Move::firstOrCreate(
+                ['name' => $moveData['move']['name'],
+                'url' => $moveData['move']['url']]
+            );
+            $moves[$moveData['move']['name']] = $move;
+         }else{
+            $move = $moves[$moveData['move']['name']];
+         }
+            
+            
+
+            foreach($moveData['version_group_details'] as $details){
+                if (!isset($moveLearnMethods[$details['move_learn_method']['name']])){
+                    $moveLearnMethod = MoveLearnMethod::firstOrCreate(
+                        ['name' => $details['move_learn_method']['name'],
+                        'url' => $details['move_learn_method']['url']]
+                    );
+                    $moveLearnMethods[$details['move_learn_method']['name']] = $moveLearnMethod;
+                }else{
+                    $moveLearnMethod = $moveLearnMethods[$details['move_learn_method']['name']];
+                }
+
+                if (!isset($versionGroups[$details['version_group']['name']])){
+                    $versionGroup = VersionGroup::firstOrCreate(
+                        ['name' => $details['version_group']['name'],
+                        'url' => $details['version_group']['url']]
+                    );
+                    $versionGroups[$details['version_group']['name']] = $versionGroup;
+                }else{
+                    $versionGroup = $versionGroups[$details['version_group']['name']];
+                }
+
+                $versionGroupdetail = VersionGroupDetail::firstOrCreate([
+                    'level_learned_at' => $details['level_learned_at'],
+                    'move_learn_method_id' => $moveLearnMethod->id,
+                    'version_group_id' => $versionGroup->id
+                ]);
+
+                PokemonMove::firstOrCreate([
+                    'pokemon_id' => $pokemon->id,
+                    'move_id' => $move->id,
+                    'version_group_detail_id' => $versionGroupdetail->id
+                ]);
+            }
+
+        }
+
+        foreach ($pokemonData['sprites'] as $key => $value) {
+            if(gettype($value)=="array"){
+                continue;
+            }
+            $sprite = Sprite::firstOrCreate([
+                'pokemon_id' => $pokemon->id,
+                'key' => $key,
+                'url' => $value
+            ]);
+        }
+
+        foreach ($pokemonData['types'] as $typeData){
+            if(!isset($types[$typeData['type']['name']])){
+                $type = Type::firstOrCreate(
+                    ['name' => $typeData['type']['name'],
+                    'url' => $typeData['type']['url']]
+                );
+                $types[$typeData['type']['name']] = $type;
+            }else{
+                $type = $types[$typeData['type']['name']];
+            }
+            
+
+            PokemonType::firstOrCreate([
+                'pokemon_id' => $pokemon->id,
+                'slot' => $typeData['slot'],
+                'type_id' => $type->id
+            ]);
+        }
+
+        foreach ($pokemonData['stats'] as $statData){
+            if(!isset($stats[$statData['stat']['name']])){
+                $stat = Stat::firstOrCreate(
+                    ['name' => $statData['stat']['name'],
+                    'url' => $statData['stat']['url']]
+                );
+                $stats[$statData['stat']['name']] = $stat;
+            }else{
+                $stat = $stats[$statData['stat']['name']];
+            }
+            
+            PokemonStat::firstOrCreate([
+                    'pokemon_id' => $pokemon->id,
+                    'base_stat' => $statData['base_stat'],
+                    'effort' => $statData['effort'],
+                    'stat_id' => $stat->id
+            ]);
+
+        }
+        $this->info("Pokemon saved: " . $pokemon->id ." => ". $pokemon->name);
         return Command::SUCCESS;
     }
 }
